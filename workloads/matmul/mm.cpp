895b5450ae466648b6687bc1425b03a61865bb0b
tutorcs <tutorcs@163.com>
2023-02-04 15:56:52 +0800

    WeChat cstutorcs ECS 201A Assignment 3

---
+++ workloads/matmul/mm.cpp
@@ -0,0 +1,43 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+
+#include <iostream>
+
+#include "matrix.h"
+
+#ifdef GEM5
+#include "gem5/m5ops.h"
+#endif
+
+
+void multiply(double* A, double* B, double* C, int size)
+{
+    for (int i = 0; i < size; i++) {
+        for (int k = 0; k < size; k++) {
+            for (int j = 0; j < size; j++) {
+                C[i * size + j] += A[i * size + k] * B[k * size + j];
+            }
+        }
+    }
+}
+
+int main()
+{
+    std::cout << "Beginning matrix multiply ..." << std::endl;
+
+#ifdef GEM5
+    m5_work_begin(0,0);
+#endif
+
+    multiply(A, B, C, SIZE);
+
+#ifdef GEM5
+    m5_work_end(0,0);
+#endif
+
+    std::cout << "Finished matrix multiply." << std::endl;
+
+    return 0;
+}
